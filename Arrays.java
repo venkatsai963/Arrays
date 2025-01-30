4fee016a93a18c184726716ba3a7b717d1ca5dbb
venkatsai963 <venkatgunji298@gmail.com>
2025-01-30 22:18:07 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+
+
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,123};
+    
+        // for(int i=0;i<5;i++)
+
+        System.out.println(a);
+        for (int i:a){
+            System.out.println(i);
+        }
+        
+        
+        
+        }
+    }
+    
+
